cdcd621326e8a5743cadb65a31fcc889cb565635
Peter Starup <pete59f1@edu.eal.dk>
2019-01-24 11:28:30 +0100

    Nye buttons til WPF

--- View-WPF/MainWindow.xaml.cs
+++ View-WPF/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Ex28_Rewriting;
 
 namespace View_WPF
 {
@@ -20,9 +21,33 @@ namespace View_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        Ex28_Rewriting.Control cont = new Ex28_Rewriting.Control();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void FindOwnerBut_Click(object sender, RoutedEventArgs e)
+        {
+            findOwnerBlock.Text = cont.FindOwnerByLastName(findOwerByLNTB.Text).LastName + cont.FindOwnerByLastName(findOwerByLNTB.Text).FirstName;
+        }
+
+        private void AllPetsBut_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Pet pet in cont.ShowPets())
+            {
+                allPetsTB.Text += pet.Name + pet.Type + pet.Breed + pet.DOB + pet.Weight + pet.OwnerId + "\n";
+            }
+        }
+
+        private void InsertOwnerBut_Click(object sender, RoutedEventArgs e)
+        {
+            cont.InsertOwner(ownerFNTB.Text, ownerLNTB.Text, ownerTLFTB.Text, ownerEmailTB.Text);
+        }
+
+        private void InsertPetBut_Click(object sender, RoutedEventArgs e)
+        {
+            cont.InsertPet(petNameTB.Text, petTypeTB.Text, petBreedTB.Text, petDOBTB.Text, petWeightTB.Text, petOwnerIdTB.Text);
         }
     }
 }
