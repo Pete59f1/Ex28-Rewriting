2c6ef53213556eceef79d4725440e73e095a02a8
Peter Starup <pete59f1@edu.eal.dk>
2019-01-30 09:00:35 +0100

    DB

---
+++ Ex28 Rewriting/IPublisher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex28_Rewriting
+{
+    public interface IPublisher
+    {
+        void RegisterSubscriber(ISubscriber observer);
+        void RemoveSubscriber(ISubscriber observer);
+        void NotifySubscribers(string message);
+    }
+}
